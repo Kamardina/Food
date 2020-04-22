81257765e5feadc9b84a4535ad96c85cbb6fff10
Kamardina <kamardina.ekaterina@gmail.com>
2020-04-22 15:55:52 +0300

    My first commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,138 +4,138 @@
 
 $(document).foundation();
 
-$(document).ready(function() {
-	var contactForm = $('#formContactFooter');
-	contactForm.validate({
-		rules: {
-			name: {
-				required: true
-			},
-			email: {
-				required: true,
-				emailfull: true
-			},
-			message: {
-				required: true
-			},
-			password: {
-				required: true
-			},
-			subject: {
-				required: true
-			}
-		},
-		messages: {
-			name: 'Type your name',
-			email: 'Type your email',
-			message: 'Type your message',
-			password: 'Type your password',
-			subject: 'Type your subject'
-		},
-		submitHandler: function(form) {
-			$(form).trigger('formvalid');
-		}
-	});
+$(document).ready(function () {
+  var contactForm = $("#formContactFooter");
+  contactForm.validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      email: {
+        required: true,
+        emailfull: true,
+      },
+      message: {
+        required: true,
+      },
+      password: {
+        required: true,
+      },
+      subject: {
+        required: true,
+      },
+    },
+    messages: {
+      name: "Type your name",
+      email: "Type your email",
+      message: "Type your message",
+      password: "Type your password",
+      subject: "Type your subject",
+    },
+    submitHandler: function (form) {
+      $(form).trigger("formvalid");
+    },
+  });
 
-	jQuery.validator.addMethod('emailfull', function(value, element) {
-		return (
-			this.optional(element) ||
-			/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(value)
-		);
-	});
-	contactForm.on('submit', function(e) {
-		e.preventDefault;
-	});
+  jQuery.validator.addMethod("emailfull", function (value, element) {
+    return (
+      this.optional(element) ||
+      /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(
+        value
+      )
+    );
+  });
+  contactForm.on("submit", function (e) {
+    e.preventDefault;
+  });
 
-		var contactForm = $('#formContact');
-		contactForm.validate({
-			rules: {
-				name: {
-					required: true
-				},
-				email: {
-					required: true,
-					emailfull: true
-				},
-				message: {
-					required: true
-				},
-				password: {
-					required: true
-				},
-				subject: {
-					required: true
-				}
-			},
-			messages: {
-				name: 'Type your name',
-				email: 'Type your email',
-				message: 'Type your message',
-				password: 'Type your password',
-				subject: 'Type your subject'
-			},
-			submitHandler: function(form) {
-				$(form).trigger('formvalid');
-			}
-		});
-	
-		jQuery.validator.addMethod('emailfull', function(value, element) {
-			return (
-				this.optional(element) ||
-				/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(value)
-			);
-		});
-		contactForm.on('submit', function(e) {
-			e.preventDefault;
-		});
+  var contactForm = $("#formContact");
+  contactForm.validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      email: {
+        required: true,
+        emailfull: true,
+      },
+      message: {
+        required: true,
+      },
+      password: {
+        required: true,
+      },
+      subject: {
+        required: true,
+      },
+    },
+    messages: {
+      name: "Type your name",
+      email: "Type your email",
+      message: "Type your message",
+      password: "Type your password",
+      subject: "Type your subject",
+    },
+    submitHandler: function (form) {
+      $(form).trigger("formvalid");
+    },
+  });
 
-	// contactForm.ajaxEmailSender({
-	// 	domain: '',
-	// 	autorizationToken:
-	// 		'',
-	// 	event: '',
-	// 	supportEmail: ''
-	// });
-    
-var e = $('.menu');
-    
-    e.find('a').click(function(){
-        var but = $(this).parent();
-        but.addClass('is-active');
-        but.siblings().removeClass('is-active');
-	 });
+  jQuery.validator.addMethod("emailfull", function (value, element) {
+    return (
+      this.optional(element) ||
+      /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(
+        value
+      )
+    );
+  });
+  contactForm.on("submit", function (e) {
+    e.preventDefault;
+  });
 
+  // contactForm.ajaxEmailSender({
+  // 	domain: '',
+  // 	autorizationToken:
+  // 		'',
+  // 	event: '',
+  // 	supportEmail: ''
+  // });
+
+  var e = $(".menu");
+
+  e.find("a").click(function () {
+    var but = $(this).parent();
+    but.addClass("is-active");
+    but.siblings().removeClass("is-active");
+  });
 });
 
-$(window).bind('resize load',function(){
-	var c = $(this).width();
-	if (c<640){
-			$('#example-menu').addClass('mobile-version');
-	}
-	else {
-		$('#example-menu').removeClass('mobile-version');
-	}
-	
+$(window).bind("resize load", function () {
+  var c = $(this).width();
+  if (c < 640) {
+    $("#example-menu").addClass("mobile-version");
+  } else {
+    $("#example-menu").removeClass("mobile-version");
+  }
 });
 
-$('.menu-icon').click(function(e){
-	e.stopPropagation();
+$(".menu-icon").click(function (e) {
+  e.stopPropagation();
 });
 
-$('body').click(function(){
-	$('.mobile-version').css('display','none');
-	
+$("body").click(function () {
+  $(".mobile-version").css("display", "none");
 });
 
-$('.js-tab-trigger').on('click', function(){
-	var tabName = $(this).data('tab'),
-		tab = $('.js-tab-content[data-tab="'+tabName+'"]');
+$(".js-tab-trigger").on("click", function () {
+  var tabName = $(this).data("tab"),
+    tab = $('.js-tab-content[data-tab="' + tabName + '"]');
 
-	$('.js-tab-trigger.active').removeClass('active');
-	$(this).addClass('active');
+  $(".js-tab-trigger.active").removeClass("active");
+  $(this).addClass("active");
 
-	$('.js-tab-content').removeClass('active');
-	tab.addClass('active');	
+  $(".js-tab-content").removeClass("active");
+  tab.addClass("active");
 });
 
 // var getAddress = {
